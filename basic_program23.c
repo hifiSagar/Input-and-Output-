53feed3aa36a99de86e11aec574ff6c22e01d6ba
SAGAR T <ssagu6661@gmail.com>
2024-10-05 18:53:55 +0530

    Add files via upload

---
+++ basic_program23.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+
+int main() 
+{ 
+    char ch;
+    scanf("%c",&ch);
+    char s;
+    scanf("%s",&s);                                     /*it have some problem*/
+    char sen;
+    scanf("%s",&sen);
+    printf("%c\n",ch);
+      printf("%s\n",s);
+        printf("%s\n",sen);
+ return 0;   
+}
